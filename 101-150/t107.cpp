8ed9167ac5a7fc280c4da55a37427bd12812dbb8
Stokish <57891693+Stokish@users.noreply.github.com>
2019-11-22 01:03:05 +0600

    Update t107.cpp

--- 101-150/t107.cpp
+++ 101-150/t107.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 int main () {
-  
-   for(int i=10; i>0; i--)
-   {
-       int j=i;
-      while(j--){
-      cout<<i;
-      }
-      cout<<endl;
-   }
-return 0;
+    int s;
+    for(int i=10; i>0; i--)
+    {
+        int j=i;
+        s+=i*j;
+        while(j--){
+            cout<<i<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<s;
+    return 0;
 }
