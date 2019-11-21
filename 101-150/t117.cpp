302d3c47d4516fd8dd9088d4052ee8b84acef005
Stokish <57891693+Stokish@users.noreply.github.com>
2019-11-22 00:20:26 +0600

    Update t117.cpp

--- 101-150/t117.cpp
+++ 101-150/t117.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 int main () {
-    double a,b=123.0,k=0;
-    while(b!=0)
+    double a,k=0;
+    while(a!=0)
      {
          cin>>a;
          k+=a;
-         b=a;
      }
      cout<<k;
 return 0;
