19818a7accdd37ad40ad0e15079b41dbcd371dd8
Stokish <57891693+Stokish@users.noreply.github.com>
2019-11-22 18:10:22 +0600

    Update t135.cpp

--- 101-150/t135.cpp
+++ 101-150/t135.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 int main () {
-    int s=0;
-    while (s<=10) {
+    int s=0, n;
+    srand(time(0));
+    n=rand()%100+11;
+    while (s<=n) {
         int a=rand()%2;
         cout<<a<<" ";
         s+=a;
